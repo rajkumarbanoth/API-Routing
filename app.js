1cae2a5a51b5880e9b7be48ce91de16853136ff6
rajkumar <rajbanoth667@gmail.com>
2022-09-05 08:27:19 +0000

    solution for API Routing

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const app = express();
 
-export.app = app.get("/", (request, response) => {
-  response, send("Home Page");
+app.get("/", (request, response) => {
+  response.send("Home Page");
 });
 
-export.app = app.get("/about", (request, response) => {
-  response, send("About Page");
+app.get("/about", (request, response) => {
+  response.send("About Page");
 });
 
 app.listen(3000);
+module.exports = app;
